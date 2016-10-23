4992a66b3251981a91aa50360091e42c524c4d0e
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2016-10-23 17:50:14 -0500

    Archivos WEB funcionales
    A partir de este último commit se ha subido completamente la primera versión funcional del juego del ahorcado.

---
+++ www/index.php
@@ -0,0 +1,7 @@
+<?php
+
+    /**
+    * Autor: Camilo Figueroa ( Crivera ) 23/10/2016
+    */
+
+    header( "location: juego.php" );
---
+++ www/juego.php
@@ -0,0 +1,25 @@
+<!-- Autor: Camilo Figueroa ( Crivera ) 23/10/2016 -->
+
+
+<html>
+    
+    <head>
+        <title></title>
+    
+        <!-- -->
+    	<link rel="stylesheet" type="text/css" href="css/estilo-general.css">    
+        <script src="js/funciones_generales.js"></script>
+        
+    </head>
+    
+    <body>
+        
+        Juego del ahorcado<br>
+        
+        <div id="gran-contenedor">
+        </div>
+
+        
+    </body>
+    
+</html>
